e4e880a208222bc14022df33befae9c604579e1c
Anar Babashov <anarbabashov.it@gmail.com>
2022-09-08 14:59:09 -0400

    Added products list to the cart page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,30 @@
+import CartContext from "../contexts/CartContext";
+
 export default function IndexPage() {
-  return <></>;
+  return <>
+      <CartContext.Consumer>
+        {({cart}) =>  
+          <ul
+          suppressHydrationWarning
+          style={{
+            display: "inline-block",
+            padding: "1em 2em",
+            margin: 0
+          }}
+          >
+            {cart.length === 0 ? (
+              <li suppressHydrationWarning>The Cart is Empty</li>
+            ) : (
+              cart.map((cartItem, index) => (
+                <li key={index} suppressHydrationWarning>
+                  {`${cartItem.name} -- ${Object.keys(cartItem.variation)
+                    .map((key) => cartItem.variation[key])
+                    .join(" + ")} `}
+                </li>
+              ))
+            )}
+          </ul>
+      }
+      </CartContext.Consumer>
+  </>;
 }
